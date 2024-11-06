18a544bf9ccdb75ca1bb3ff3968292f8906fa414
Léo <leogaudinoff@gmail.com>
2024-11-06 19:15:19 +0100

    feat: put country fields

--- client/src/service/specific.js
+++ client/src/service/specific.js
@@ -27,13 +27,14 @@ export const API_URL =
 // - csv.js
 export const boxFields = {
 	project: { type: String, required: true },
-	division: { type: String, required: false },
-	district: { type: String, required: true },
-	zone: { type: String, required: false },
-	school: { type: String, required: true },
+	state: { type: String, required: true },
+	lgea: { type: String, required: true },
+	school: { type: String, required: false },
+	schoolCode: { type: String, required: false },
 	htName: { type: String, required: false },
 	htPhone: { type: String, required: false },
-	schoolCode: { type: String, required: false },
+	ssoName: { type: String, required: false },
+	ssoPhone: { type: String, required: false },
 };
 
 // Minimal fields that are used to differentiate boxes (e.g. for updating coordinates)
--- server/models/boxes.model.js
+++ server/models/boxes.model.js
@@ -3,13 +3,14 @@ const Schema = mongoose.Schema;
 
 const boxFields = {
 	project: { type: String, required: true },
-	division: { type: String, required: false },
-	district: { type: String, required: true },
-	zone: { type: String, required: false },
-	school: { type: String, required: true },
+	state: { type: String, required: true },
+	lgea: { type: String, required: true },
+	school: { type: String, required: false },
+	schoolCode: { type: String, required: false },
 	htName: { type: String, required: false },
 	htPhone: { type: String, required: false },
-	schoolCode: { type: String, required: false },
+	ssoName: { type: String, required: false },
+	ssoPhone: { type: String, required: false },
 };
 
 const Box = new Schema(
