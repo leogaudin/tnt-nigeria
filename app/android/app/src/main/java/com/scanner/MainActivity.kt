521a3b08076eea209f6ec56ef278d44e4e480cca
Léo <leogaudinoff@gmail.com>
2025-02-28 19:43:10 +0100

    build: updates bis

--- app/android/app/src/main/java/com/scanner/MainActivity.kt
+++ app/android/app/src/main/java/com/scanner/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tntnigeria.scanner
+package com.gaudin.dainigeriatnt
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- app/android/app/src/main/java/com/scanner/MainApplication.kt
+++ app/android/app/src/main/java/com/scanner/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.tntnigeria.scanner
+package com.gaudin.dainigeriatnt
 
 import android.app.Application
 import com.facebook.react.PackageList
